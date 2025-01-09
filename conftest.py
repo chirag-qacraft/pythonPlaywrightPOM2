1a2f32ee756d6dce1e9ef611de9ab9257431647e
chirag-qacraft <chirag@qacraft.com>
2025-01-09 12:11:59 +0530

    commit

--- conftest.py
+++ conftest.py
@@ -5,10 +5,9 @@ from playwright.async_api import async_playwright
 @pytest_asyncio.fixture(scope="function")
 async def browser_setup():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
-        context = await browser.new_context()
+        browser = await p.chromium.launch(headless=False, args=["--start-maximized"])
+        context = await browser.new_context(no_viewport=True)
         page = await context.new_page()
-        await page.set_viewport_size({"width": 1920, "height": 1080})
         base_url = "https://practice.expandtesting.com/login"
         await page.goto(base_url)
         await page.wait_for_load_state("domcontentloaded")
